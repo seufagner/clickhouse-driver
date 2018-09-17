c0fd5ae66908561296ff03c11e86f7e0185d2599
Konstantin Lebedev <kostyan.lebedev@gmail.com>
2018-09-17 20:24:32 +0300

    String columns optimization

--- clickhouse_driver/columns/stringcolumn.py
+++ clickhouse_driver/columns/stringcolumn.py
@@ -1,42 +1,50 @@
 
 from .. import errors
-from ..reader import read_binary_bytes, read_binary_bytes_fixed_len
-from ..writer import write_binary_bytes, write_binary_bytes_fixed_len
+from ..reader import read_binary_bytes, read_binary_bytes_fixed_len, read_varint
+from ..writer import write_binary_bytes, write_binary_bytes_fixed_len, write_varint, _byte
 from ..util import compat
-from .base import CustomItemColumn
+from .base import Column
 
+from codecs import utf_8_decode, utf_8_encode
 
-class String(CustomItemColumn):
+class String(Column):
     ch_type = 'String'
     py_types = compat.string_types
 
     # TODO: pass user encoding here
 
-    def try_encode(self, value):
-        if not isinstance(value, bytes):
-            return value.encode('utf-8')
-        return value
+    def prepare_null(self, value):
+        if self.nullable and value is None:
+            return '', True
 
-    def try_decode(self, value):
-        try:
-            return value.decode('utf-8')
-        except UnicodeDecodeError:
-            # Do nothing. Just return bytes.
-            pass
+        else:
+            return value, False
 
-        return value
+    def write_items(self, items, buf):
+        for value in items:
+            if not isinstance(value, bytes):
+                value = utf_8_encode(value)[0]
 
-    def read(self, buf):
-        return self.try_decode(read_binary_bytes(buf))
+            write_varint(len(value), buf)
+            buf.write(value)
 
-    def _read_null(self, buf):
-        self.read(buf)
+    def read_items(self, n_items, buf):
+        items = [None] * n_items
+        i = 0
+        while i < n_items:
+            length = read_varint(buf)
+            value = buf.read(length)
 
-    def write(self, value, buf):
-        write_binary_bytes(self.try_encode(value), buf)
+            try:
+                value = utf_8_decode(value)[0]
+            except UnicodeDecodeError:
+                # Do nothing. Just return bytes.
+                pass
 
-    def _write_null(self, buf):
-        self.write('', buf)
+            items[i] = value
+            i += 1
+
+        return items
 
 
 class FixedString(String):
@@ -46,20 +54,42 @@ class FixedString(String):
         self.length = length
         super(FixedString, self).__init__(**kwargs)
 
-    def read(self, buf):
-        text = self.try_decode(read_binary_bytes_fixed_len(buf, self.length))
-        if isinstance(text, bytes):
-            strip = b'\x00'
-        else:
-            strip = '\x00'
-        return text.strip(strip)
+    def read_items(self, n_items, buf):
+        items = [None] * n_items
+        items_buf = buf.read(self.length * n_items)
 
-    def write(self, value, buf):
-        try:
-            value = self.try_encode(value)
-            write_binary_bytes_fixed_len(value, buf, self.length)
-        except ValueError:
-            raise errors.TooLargeStringSize()
+        i = 0
+        buf_pos = 0
+        while i < n_items:
+            value = items_buf[buf_pos:buf_pos + self.length]
+            try:
+                value = utf_8_decode(value)[0]
+                value = value.rstrip('\x00')
+            except UnicodeDecodeError:
+                value = value.rstrip(b'\x00')
+
+            items[i] = value
+            i += 1
+            buf_pos += self.length
+
+        return items
+
+    def write_items(self, items, buf):
+        items_buf = bytearray(self.length * len(items))
+        items_buf_view = memoryview(items_buf)
+        buf_pos = 0
+
+        for value in items:
+            if not isinstance(value, bytes):
+                value = value.encode('utf-8')
+
+            if self.length < len(value):
+                raise errors.TooLargeStringSize()
+
+            items_buf_view[buf_pos:buf_pos + min(self.length, len(value))] = value
+            buf_pos += self.length
+
+        buf.write(items_buf)
 
 
 def create_fixed_string_column(spec):
